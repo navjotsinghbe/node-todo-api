dd4366490be457db25aefcb198e3cc9c835e3ed3
navjot singh <navjotsinghbe@gmail.com>
2018-06-22 07:37:31 +1000

    post request

--- server/server.js
+++ server/server.js
@@ -20,12 +20,19 @@ app.post('/todos',(req,res)=>{
     console.log(req.body)
 })
 app.get('/todos',(req,res)=>{
-
+    Todo.find().then((todos)=>{
+        res.send({
+            todos,
+        })
+    },(e)=>{
+        res.status(400).send(e)
+    })
 })
 var port = 3000
 app.listen(port, () => {
     console.log(`started on port ${port}`)
 })
 
+module.exports = {app};
 
 
---
+++ server/tests/server.test.js
@@ -0,0 +1,10 @@
+const expect = require('expect')
+const request = require('supertest')
+
+const {app} = require('./../server')
+const {Todo} = require('./../models/todo')
+
+
+describe('POST /todos', ()=>{
+    
+})
